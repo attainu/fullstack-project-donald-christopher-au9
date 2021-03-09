a883f767bd94b6244b3cec5273838a82b245e2c7
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2021-03-09 15:57:45 +0530

    mar 9th

--- frontend/src/Mainrouter.js
+++ frontend/src/Mainrouter.js
@@ -4,13 +4,17 @@ import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 import Homepage from './Components/Homepage/Homepage'
 import Loginpage from './Components/Login components/Loginpage';
+import Navbar from './Components/Navbar/Navbar'
 
+import Doctorlist from './Components/Doctorlist/Doctorlist';
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-<Route path='/' exact component={Homepage}/>
-<Route path='/login' component={Loginpage}/>
+      <Navbar/>
+        <Route path='/' exact component={Homepage}/>
+        <Route path='/login' component={Loginpage}/>
+        <Route path='/doctorlist' component={Doctorlist}/>
       </BrowserRouter>
     </div>
   )
